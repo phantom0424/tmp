c57c2f47a354f1e2af6562b4875e72d3d050e0bf
phantom <563789445@qq.com>
2020-02-29 14:21:44 +0800

    ff

--- src/qrdownload/qrdownload.js
+++ src/qrdownload/qrdownload.js
@@ -2,10 +2,10 @@ var md = new MobileDetect(window.navigator.userAgent);
 
 var detectDom = document.getElementById('detect');
 
-var itemList = ['ipad', 'iphone', 'android', 'wechat'];
+var itemList = ['ipad', 'ios', 'android', 'wechat'];
 for (item of itemList) {
     var itemDom = document.createElement('div');
-    itemDom.append('is d' + item + ': ')
+    itemDom.append('is ' + item + ': ')
     itemDom.append(md.is(item))
     detectDom.appendChild(itemDom)
 }
