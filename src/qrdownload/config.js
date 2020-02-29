e59571ff0b0c3dd0b59e19fa7323210faf5c090b
phantom <563789445@qq.com>
2020-02-29 19:30:02 +0800

    家长、学生、教师版二维码跳转页实现

---
+++ src/qrdownload/config.js
@@ -0,0 +1,14 @@
+window.downloadUrls = {
+    teacher: {
+        ios: 'https://apps.apple.com/cn/app/%E4%BC%A0%E8%80%8C%E4%B9%A0%E6%95%99%E5%B8%88%E7%89%88/id1492681880',
+        iosHD: 'https://apps.apple.com/cn/app/%E4%BC%A0%E8%80%8C%E4%B9%A0%E6%95%99%E5%B8%88hd%E7%89%88/id1484750466'        
+    },
+    parent: {
+        android: 'https://www.ecaiedu.com/#/android-download',
+        ios: 'https://apps.apple.com/cn/app/%E4%BC%A0%E8%80%8C%E4%B9%A0%E5%AE%B6%E9%95%BF%E7%89%88/id1492206581',
+    },
+    student: {
+        android: 'https://www.ecaiedu.com/#/android-download?type=student',
+        ios: 'https://apps.apple.com/cn/app/%E4%BC%A0%E8%80%8C%E4%B9%A0%E5%AD%A6%E7%94%9F%E7%89%88/id1488697201',
+    }
+}
