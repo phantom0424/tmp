d922c14fb0cb87d41f19ae8a71671d1c8371d920
phantom <563789445@qq.com>
2020-03-01 23:22:26 +0800

    下载首页增加二维码动态生成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+var QRCode = require('qrcode')
+
+var roles = ['teacher', 'parent', 'student'];
+
+for(role of roles) {
+    var canvas = document.getElementById('qrcode-' + role);
+    QRCode.toCanvas(canvas, 'https://www.ecaiedu.com/qrdownload/' + role + '.html', function (error) {
+        if (error) console.error(error)
+        console.log('success!');
+    })
+}
+
+
 console.log('index')
