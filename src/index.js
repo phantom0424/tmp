58d41d8d07ee5eef7adb95989c3d6c2da9ec6d24
phantom <563789445@qq.com>
2020-03-02 23:20:36 +0800

    下载首页完整实现

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 var QRCode = require('qrcode')
 
-var roles = ['teacher', 'parent', 'student'];
+var roles = [
+    // 'teacher',
+    // 'parent',
+    // 'student'
+];
 
 for(role of roles) {
     var canvas = document.getElementById('qrcode-' + role);
-    QRCode.toCanvas(canvas, 'https://www.ecaiedu.com/qrdownload/' + role + '.html', function (error) {
+    QRCode.toCanvas(canvas, 'https://www.ecaiedu.com/qrdownload/' + role + '.html', { width: 124, margin: 2 }, function (error) {
         if (error) console.error(error)
         console.log('success!');
     })
